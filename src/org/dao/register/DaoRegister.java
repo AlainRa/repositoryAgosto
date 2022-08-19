0d7e8d5cb90e2e21fd0e98a91b0fd3d36c7856bf
Ing. Alain Ramirez Garcia <alain.r.g.23@gmail.com>
2022-08-19 10:58:16 -0500

    CREACION DE PROYECTO

---
+++ src/org/dao/register/DaoRegister.java
@@ -0,0 +1,17 @@
+package org.dao.register;
+
+import java.util.List;
+
+import org.modelo.register.Register;
+
+/*
+ * INTERFACE: NO SE PROGRAMA, SOLO SE AGREGAN METODOS QUE SE VAN A IMPLEMENTAR.
+ * */
+
+public interface DaoRegister {
+
+	public void addRegister(Register register);
+	public List<Register> listRegisters();
+	public Register getRegisterById(int id);
+	public void deleteRegegister(int id);
+}
